f0bac1c0ac6c91afde7954e3d6b445dd7fd536a4
milzj <60394891+milzj@users.noreply.github.com>
2022-04-06 20:34:17 +0200

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,16 @@
 from pathlib import Path
 import sys
 
-
-import pytest
-import numpy as np
-
 here = Path(__file__).parent
 sys.path.insert(0, str(here.parent) + "/problem")
 sys.path.insert(0, str(here.parent) + "/prox")
 sys.path.insert(0, str(here.parent) + "/algorithms")
 sys.path.insert(0, str(here.parent) + "/base")
+
+import pytest
+import numpy as np
+
+
 
 
 def pytest_runtest_setup(item):
