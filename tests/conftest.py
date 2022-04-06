4ec16091c8a4abe18cee8f40044cf91a85c0dfe9
milzj <60394891+milzj@users.noreply.github.com>
2022-04-06 19:58:08 +0200

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,14 @@
 import pytest
 import numpy as np
+from pathlib import Path
+import sys
+
+here = Path(__file__).parent
+sys.path.insert(0, str(here.parent) + "/problem")
+sys.path.insert(0, str(here.parent) + "/prox")
+sys.path.insert(0, str(here.parent) + "/algorithms")
+sys.path.insert(0, str(here.parent) + "/base")
+
 
 def pytest_runtest_setup(item):
 	np.random.seed(1234)
